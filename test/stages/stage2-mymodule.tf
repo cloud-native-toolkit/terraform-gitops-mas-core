74e5207a7a74d053a8a7b585637547c207340511
Sean Sundberg <seansund@us.ibm.com>
2022-11-14 13:02:37 -0600

    Initial version
    closes cloud-native-toolkit/software-everywhere#545
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -3,7 +3,14 @@ module "gitops_module" {
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
+  server_name = module.gitops.server_name
+
+  catalog_name = module.toolkit_catalog.name
+  entitlement_key = var.cp_entitlement_key
+  uds_contact_email = "someone@somewhere.com"
+  uds_contact_first_name = "Test"
+  uds_contact_last_name = "User"
+  mongodb_storage_class = module.storage_manager.block_storage_class
+  uds_storage_class = module.storage_manager.block_storage_class
 }
