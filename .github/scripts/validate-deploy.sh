394b5d087a1e363e16bb9123ce05faa336035534
Sean Sundberg <seansund@us.ibm.com>
2023-01-10 11:39:49 -0600

    Simplifies validation logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -59,10 +59,12 @@ check_k8s_resource "${NAMESPACE}" deployment masauto-operator-controller-manager
 
 check_k8s_resource "${NAMESPACE}" secret ibm-entitlement-key || exit 1
 
+check_k8s_resource ibm-common-services deployment cert-manager-controller || exit 1
+
 check_k8s_resource ibm-sls deployment sls-api-licensing || exit 1
 
 check_k8s_resource ibm-common-services deployment user-data-services-operator || exit 1
-check_k8s_resource ibm-common-services deployment ibm-licensing-operator || exit 1
+#check_k8s_resource ibm-common-services deployment ibm-licensing-operator || exit 1
 
 check_k8s_resource mas-inst1-core suite inst1 || exit 1
 
